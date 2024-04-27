b98b9fced0327d765b96a0a1e14315fc8b350e41
Jamiu Okanlawon <developerjamiu@gmail.com>
2024-04-27 12:34:30 +0100

    feat: implemented

--- lib/repositories/article_idea_repository.dart
+++ lib/repositories/article_idea_repository.dart
@@ -1,9 +1,37 @@
+import 'package:google_generative_ai/google_generative_ai.dart';
+
 class ArticleIdeaRepository {
+  /// Inject this dependency in a real app
+  /// The key should be hidden in a real app
+  final model = GenerativeModel(
+    model: 'gemini-pro',
+    apiKey: 'api-key',
+  );
+
+  /// This should be properly constructed in a real app
+  final initialPrompt =
+      "Return a 5 list of article ideas with the following rules and payload. If seoEnabled is true in the payload, make the list seo optimized. Return only the list without any title or description and without any form of numbering or formatting; just plain text.";
+
   Future<List<String>> fetchArticleIdeas({
     required String query,
     bool seoEnabled = false,
   }) async {
-    await Future.delayed(const Duration(seconds: 1));
-    return List.generate(10, (index) => 'Article Idea $index');
+    try {
+      final prompt =
+          '$initialPrompt payload: {query: $query, seoEnabled: $seoEnabled}. ';
+
+      final response = await model.generateContent(
+        [Content.text(prompt)],
+      );
+
+      if (response.text == null) return [];
+
+      final List<String> splitResult = (response.text as String).split('\n');
+
+      return splitResult.map((result) => result.replaceAll('- ', '')).toList();
+    } catch (ex) {
+      /// In a real app, you'll handle all possible errors
+      return [];
+    }
   }
 }
